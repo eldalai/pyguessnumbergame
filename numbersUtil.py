a6f3aeed09863284acaaff900f49c92141022ef7
Gabriel <eldalai@gmail.com>
2014-11-07 01:33:42 -0300

    check numbers are OK

--- numbersUtil.py
+++ numbersUtil.py
@@ -6,7 +6,7 @@ def goodNumbers(number, other):
 	goods = 0
 	for indexNumber in xrange(0,4):
 		for indexOther in xrange(0,4):
-			print " compare (%d) %s with (%d) %s " % (indexNumber, listNumber[indexNumber], indexOther, listOther[indexOther] )
+			print "goods: compare (%d) %s with (%d) %s " % (indexNumber, listNumber[indexNumber], indexOther, listOther[indexOther] )
 			if listNumber[indexNumber] == listOther[indexOther] and indexNumber == indexOther:
 				print "GOOD!"
 				goods = goods + 1 
@@ -19,9 +19,23 @@ def regularNumbers(number, other):
 	regulars = 0
 	for indexNumber in xrange(0,4):
 		for indexOther in xrange(0,4):
-			print " compare (%d) %s with (%d) %s " % (indexNumber, listNumber[indexNumber], indexOther, listOther[indexOther] )
+			print "regulars: compare (%d) %s with (%d) %s " % (indexNumber, listNumber[indexNumber], indexOther, listOther[indexOther] )
 			if listNumber[indexNumber] == listOther[indexOther] and indexNumber != indexOther:
 				print "REGULAR!"
 				regulars = regulars + 1 
 	return regulars
 
+def isCorrect(number):
+	listNumber = list(number)
+	for indexNumber in xrange(0, 3):
+		if listNumber[indexNumber] < "0" or listNumber[indexNumber] > "9" :
+				print "is not Correct, %s not a number" % listNumber[indexNumber] 
+				return False
+
+		for indexNumber2 in xrange(indexNumber+1, 4):
+			print "isCorrect: compare (%d) %s with (%d) %s " % (indexNumber, listNumber[indexNumber], indexNumber2, listNumber[indexNumber2] )
+			if listNumber[indexNumber] == listNumber[indexNumber2] and indexNumber != indexNumber2:
+				print "is not Correct"
+				return False
+	print "is correct"
+	return True
--- pyguessnumbergameTest.py
+++ pyguessnumbergameTest.py
@@ -27,6 +27,17 @@ class Test(unittest.TestCase):
     	goodNumbers = numbersUtil.regularNumbers(mynumber, otherQuestion)
     	self.assertEqual(2, goodNumbers, "shoud be 2 good Numbers because 1 and 2 are not in good place")
 
+    def testNumberIsCorrect(self):
+    	number = "1232"
+    	self.assertFalse( numbersUtil.isCorrect( number ) )	
+    	number = "4234"
+    	self.assertFalse( numbersUtil.isCorrect( number ) )	
+    	number = "1234"
+    	self.assertTrue( numbersUtil.isCorrect( number ) )	
+    	number = "1A34"
+    	self.assertFalse( numbersUtil.isCorrect( number ) )	
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()    	
